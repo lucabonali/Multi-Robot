a8f4da2b71df313cdbad64f58a95e20980034c62
Luca <lucabonali12@gmail.com>
2019-03-26 18:33:41 +0100

    gytufy

--- Agent.py
+++ Agent.py
@@ -23,6 +23,7 @@ class Agent():
             return node
         else:
             self.toExpand.append(node)
+            self.setHeuristic(node)
             listPosition = self.getListOfPosition(mapChar)
             return self.expand(node, listPosition, mapChar)
 
@@ -44,7 +45,9 @@ class Agent():
                     if not value == self.wallString:
                         child = Node(father=node, xCoord=xCoord, yCoord=yCoord, value=value)
                         node.addChildren(child)
-                        self.toExpand.append(child)
+                        self.setHeuristic(child)
+                        self.updateFrontier(child)
+                        #self.toExpand.append(child)
                 except:
                     pass
         for i in range(len(node.children)):
@@ -64,3 +67,22 @@ class Agent():
             return path
         path.addNode(node)
         return self.constructPath(node.father, path)
+
+    def setHeuristic(self, node):
+        heuristic = self.getNearTarget(node.xCoord, node.yCoord)
+        node.heuristic = heuristic
+
+    def getNearTarget(self,x,y):
+        nearValues = []
+        for i in range(len(self.targets)):
+            nearValues.append(abs((self.targets[i][0]-x)+(self.targets[i][1]-y)))
+        nearValues.sort()
+        return nearValues[0]
+
+    def updateFrontier(self, child):
+        h = child.heuristic
+        for i in range(len(self.toExpand)):
+            if self.toExpand[i].heuristic >= h:
+                self.toExpand.insert(i,child)
+                return
+        self.toExpand.append(child)
